aa94345070bfefd745214bd115a21eb3a3fc816f
Georgia Salanti <georgia.salanti@gmail.com>
2019-08-16 14:55:27 +0200

    Revert "edits and comments from Georgia"

--- R/simulateDRsplinedataOR.R
+++ R/simulateDRsplinedataOR.R
@@ -1,13 +1,10 @@
-
+#This function simulate ns studies for dose of doserange with splines. The coefficients from the two dose transformations
+# are beta1 and beta2 and we assume a single tau for their RE
+# p0 is the event rate at dose 0 and it is set by the simulations according to the maximum allowed value
+# samplesize is the average sample size per study arm
 
 
 simulateDRsplinedataOR.fun=function(beta1.pooled=0.01,beta2.pooled=0.02,tau=0.001,ns=20,doserange=c(1, 10),samplesize=200,p0=0.1){ #
-
-  # This function simulates ns studies for dose of doserange with splines. The coefficients from the two dose transformations
-  # are beta1 and beta2 and we assume a single tau for their random effects
-  # p0 is the event rate at dose 0 and it is set by the simulations according to the maximum allowed value
-  # samplesize is the average sample size per study arm
-  # The function assumes 3 dose levels per study, the lowest of which is zero.
 
   library(rms)
 
@@ -21,17 +18,21 @@ simulateDRsplinedataOR.fun=function(beta1.pooled=0.01,beta2.pooled=0.02,tau=0.00
   #nr of observations: I assume each study has 0 dose and 3 levels of dose
   nobs<-ns*3
 
+  #the event rate in the zero dose has a maximum limit at p0<1/RR
+  # maxlogRR<- (beta1.pooled+2*tau)*max(trans.d[,1]) +(beta2.pooled+2*tau)*max(trans.d[,2])#
+  # maxRR<-exp(maxlogRR)
+  # p0<-0.5/maxRR#set p0 to be half the maximum allowed, just to be on the safe side!
 
-  #create the dose-specific logOR, cases and controls
+
+  #create the dose-specific logRR, cases and controls
+
   beta1<-c(sapply(rnorm(ns,beta1.pooled,tau),rep,3)) #random effects of the slopes
   beta2<-c(sapply(rnorm(ns,beta2.pooled,tau),rep,3)) #random effects of the slopes
-  logOR<- beta1*trans.d[,1]+beta2*trans.d[,2]   #derive study-specific logOR using regression
-
   uniquess<-round(runif(ns,samplesize-20,samplesize+20))#sample size in study arm of zero dose
   cases0<-rbinom(ns,uniquess,p0)#events per study at zero dose
   ss<-c(sapply(uniquess,rep,3)) #sample size per study arm
   c0<-c(sapply(cases0,rep,3))
-
+  logOR<- beta1*trans.d[,1]+beta2*trans.d[,2]   #derive study-specific logRR using regression
   OR<-exp(logOR)
 
   p0hat <- cases0/uniquess
